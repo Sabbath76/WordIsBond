0a0387939096f7f16dfef0bba0bce8a705fb983c
Sabbath76 <tomberry76@hotmail.com>
2013-11-14 10:56:38 +0000

    Media player tweaks and fixes
    Multiple tweaks on an item
    Better stream control

---
+++ testapp/TrackInfo.h
@@ -0,0 +1,13 @@
+//
+//  TrackInfo.h
+//  WordIsBond
+//
+//  Created by Jose Lopes on 09/11/2013.
+//  Copyright (c) 2013 Tom Berry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TrackInfo : NSObject
+
+@end
---
+++ testapp/TrackInfo.m
@@ -0,0 +1,13 @@
+//
+//  TrackInfo.m
+//  WordIsBond
+//
+//  Created by Jose Lopes on 09/11/2013.
+//  Copyright (c) 2013 Tom Berry. All rights reserved.
+//
+
+#import "TrackInfo.h"
+
+@implementation TrackInfo
+
+@end
